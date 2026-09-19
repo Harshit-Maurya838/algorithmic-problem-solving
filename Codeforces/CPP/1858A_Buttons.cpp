15d2d6c6ec25dc26cfe3c92b9f99b83b8636a52c
Harshit-Maurya838 <44harshitmaurya@gmail.com>
2026-09-19 23:09:48 +0530

    solve(cf): 1858A - Buttons

---
+++ Codeforces/CPP/1858A_Buttons.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    long long a, b, c;
+    cin >> a >> b >> c;
+
+    if(c % 2 == 0){
+        if(a > b){
+            cout << "First\n";
+        }else{
+            cout << "Second\n";
+        }
+    }else{
+        if(a >= b){
+            cout << "First\n";
+        }else{
+            cout << "Second\n";
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+    
+    return 0;
+}
---
+++ Codeforces/Java/Solution1858A.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution1858A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0){
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+
+            if(c % 2 == 0){
+                if(a > b){
+                    System.out.println("First");
+                }else{
+                    System.out.println("Second");
+                }
+            }else{
+                if(a >= b){
+                    System.out.println("First");
+                }else{
+                    System.out.println("Second");
+                }
+            }
+        }
+        sc.close();
+    }
+}
